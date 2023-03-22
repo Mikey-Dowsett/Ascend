536bdabf14da139ee780388d13fad767c52f06ee
Fangpower <mikeydowsett@gmail.com>
2023-03-22 10:04:01 -0500

    More saving and loading

--- Assets/Scripts/IconManager.cs
+++ Assets/Scripts/IconManager.cs
@@ -7,17 +7,35 @@ Sets the icons for the balloon, defender, background, and the theme for the obst
 */
 public class IconManager : MonoBehaviour
 {
+    const string BALLOON = "BalloonSprite";
+    const string BACKGROUND = "BackgroundSprite";
+
     [SerializeField] SpriteRenderer balloonSR;
     [SerializeField] SpriteRenderer backgroundSR;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey(BALLOON))
+        {
+            Sprite sp = Resources.Load<Sprite>("Art/Balloon/" + PlayerPrefs.GetString(BALLOON));
+            balloonSR.sprite = sp;
+        }
+        if (PlayerPrefs.HasKey(BACKGROUND))
+        {
+            Sprite sp = Resources.Load<Sprite>("Art/Background/" + PlayerPrefs.GetString(BACKGROUND));
+            backgroundSR.sprite = sp;
+        }
+    }
 
     public void SetBalloon(Sprite newSprite)
     {
         balloonSR.sprite = newSprite;
+        PlayerPrefs.SetString(BALLOON, newSprite.name);
     }
 
     public void SetBackground(Sprite newSprite)
     {
         backgroundSR.sprite = newSprite;
-        print("SR Set");
+        PlayerPrefs.SetString(BACKGROUND, newSprite.name);
     }
 }
